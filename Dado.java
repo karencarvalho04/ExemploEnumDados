b25bdca18993797758cc7d6b989be4a7e9ae4e8b
Karen Carvalho <86808543+karencarvalho04@users.noreply.github.com>
2023-10-02 10:25:49 -0300

    Create Dado.java

---
+++ Dado.java
@@ -0,0 +1,7 @@
+import java.security.SecureRandom;
+
+public class Dado {
+
+    //cria um gerados seguro de números aleatórios para uso no método JogarDados
+    public static final SecureRandom randomNumbers = new SecureRandom();
+}
