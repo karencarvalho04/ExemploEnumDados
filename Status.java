f36cad8f97a590359f329e298e1b3c500a3dd20b
Karen Carvalho <86808543+karencarvalho04@users.noreply.github.com>
2023-10-02 10:26:07 -0300

    Create Status.java

---
+++ Status.java
@@ -0,0 +1,15 @@
+//tipo enum com constantes que representam o estado do jogo
+
+public enum Status {
+    CONTINUAR,
+    VENCEDOR,
+    PERDEDOR;
+
+    //constantes que representam lançamentos comuns dos dados
+    public static final int SNAKE_EYES = 2;
+    public static final int TREY = 3;
+    public static final int SEVEN = 7;
+    public static final int YO_LEVEN = 11;
+    public static final int BOX_CARS = 12;
+
+}
